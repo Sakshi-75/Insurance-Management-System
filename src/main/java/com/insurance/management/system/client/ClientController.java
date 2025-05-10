20d4cf3a3a3685b30faf3fd1c3be1f9ed64778ac
Sakshi <sakshi_jain75@gmail.com>
2025-05-11 03:29:22 +0530

    upload files

---
+++ src/main/java/com/insurance/management/system/client/ClientController.java
@@ -0,0 +1,20 @@
+package com.insurance.management.system.client;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/client")
+public class ClientController {
+
+    @Autowired
+    ClientDetailsRepository clientDetailsRepository;
+
+    @PostMapping(value = "/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Long createClient(@RequestBody ClientDetails clientDetails) {
+        return clientDetailsRepository.save(clientDetails).getId();
+    }
+
+}
---
+++ src/main/java/com/insurance/management/system/client/ClientDetailsRepository.java
@@ -0,0 +1,6 @@
+package com.insurance.management.system.client;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientDetailsRepository extends JpaRepository<ClientDetails, Long> {
+}
